06eebcd5109284d4b808882d1c0162d318f345f7
swayam suryavanshi <codingswayam24@gmail.com>
2024-02-09 15:32:28 +0530

    Basic setup

---
+++ script.js
@@ -0,0 +1,41 @@
+// Function to capitalize all text in the textarea
+function capitalizeAllText() {
+    // Code to capitalize all text
+    // ...
+}
+
+// Function to convert all text to lowercase in the textarea
+function lowerAllText() {
+    // Code to convert all text to lowercase
+    // ...
+}
+
+// Function to capitalize the first character of each word in the textarea
+function capFirstChar() {
+    // Code to capitalize the first character of each word
+    // ...
+}
+
+// Function to copy the content of the textarea to the clipboard
+function copyToClipboard() {
+    // Code to copy content to clipboard
+    // ...
+}
+
+// Function to get character count in the textarea
+function getCharCount() {
+    // Code to calculate character count
+    // ...
+}
+
+// Function to get word count in the textarea
+function getWordCount() {
+    // Code to calculate word count
+    // ...
+}
+
+// Function to save the content of the textarea
+function saveContent() {
+    // Code to save content
+    // ...
+}
